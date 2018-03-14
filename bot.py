6c8c6cf440e9b56171dc909aa5cee0b2496d4746
Krupnik Sergey <krupnik35@ya.ru>
2018-03-14 16:39:27 +0300

    Post class with header works

--- bot.py
+++ bot.py
@@ -1,23 +1,32 @@
 import telegram
 from time import sleep
 
+from postClass import *
+
 # from tendo import singleton
 # me = singleton.SingleInstance()
 
 TOKEN = '582293326:AAG-1JSt4WHDXE9kMu4KFs7pghcIWKFdFE0'
 ChannelId = -1001392228565
+BoldPrefix = '<b>'
+BoldPostfix = '</b>'
 
 bot = telegram.Bot(token=TOKEN)
 
-def post_text(text):
+def post_text(post):
+
+    message = BoldPrefix
+    message += post.header
+    message += BoldPostfix + '\n\n'
+    message += post.body
+
     try:
-        bot.send_message(ChannelId, text=text)
+        bot.send_message(ChannelId, text=message, parse_mode='HTML')
     except Exception as ex:
         print('post_text: exception: ' + str(ex))
 
 
 def post_picture(picture_file, caption=''):
-
     try:
         bot.sendPhoto(ChannelId, photo=open(picture_file, 'rb'), caption=caption)
     except Exception as ex:
@@ -29,16 +38,16 @@ def get_new_text():
 
 
 def main():
-    while True:
-        try:
-            text = get_new_text()
-            if text != '':
-                post_text(text)
-        except Exception as ex:
-            print('main: exception: ' + str(ex))
-        sleep(10)
+    post_text(Post("Header", "Text"))
+    # while True:
+    #     try:
+    #         text = get_new_text()
+    #         if text != '':
+    #             post_text(text)
+    #     except Exception as ex:
+    #         print('main: exception: ' + str(ex))
+    #     sleep(10)
 
 
 if __name__ == '__main__':
     main()
-
---
+++ postClass.py
@@ -0,0 +1,4 @@
+class Post:
+    def __init__(self, header, body):
+        self.header = header
+        self.body = body
