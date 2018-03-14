fa97b0ca4edb8a93d696e1c29751003200110438
Krupnik Sergey <krupnik35@ya.ru>
2018-03-14 15:30:55 +0300

    Bot minimal functions

---
+++ bot.py
@@ -0,0 +1,44 @@
+import telegram
+from time import sleep
+
+# from tendo import singleton
+# me = singleton.SingleInstance()
+
+TOKEN = '582293326:AAG-1JSt4WHDXE9kMu4KFs7pghcIWKFdFE0'
+ChannelId = -1001392228565
+
+bot = telegram.Bot(token=TOKEN)
+
+def post_text(text):
+    try:
+        bot.send_message(ChannelId, text=text)
+    except Exception as ex:
+        print('post_text: exception: ' + str(ex))
+
+
+def post_picture(picture_file, caption=''):
+
+    try:
+        bot.sendPhoto(ChannelId, photo=open(picture_file, 'rb'), caption=caption)
+    except Exception as ex:
+        print('post_picture: failed to send photo: ' + str(ex))
+
+
+def get_new_text():
+    return ''
+
+
+def main():
+    while True:
+        try:
+            text = get_new_text()
+            if text != '':
+                post_text(text)
+        except Exception as ex:
+            print('main: exception: ' + str(ex))
+        sleep(10)
+
+
+if __name__ == '__main__':
+    main()
+
